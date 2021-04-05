751a0d8b28e18882ae4d1d45a073591a235a7bed
DMITRY LOKHMATOV <maloll777@gmail.com>
2021-04-05 22:53:56 +0300

    Обновлен класс Product добавлены функции для поиска описания и изображения в БД

--- TRIOL2_lib.py
+++ TRIOL2_lib.py
@@ -1,18 +1,18 @@
 import requests
-
+import sqlite3
+from CONFIG import DataBase
 
 class Triol:
     # класс для работы с amma.pet и добавления данных о товаре в БД
-
     def __init__(self, item):
         self.id_item = item
-        self.link = self.get_page_product()
+        self.link = self.get_page_product_amma()
         if self.link:
-            self.name, self.description, self.image = self.get_info_item()
+            self.name, self.description, self.image = self.get_info_item_amma()
         else:
             self.name = 'Товар не найден'
 
-    def get_page_product(self):
+    def get_page_product_amma(self):
         # поиск товара по id на сайте amma.pet
         # возвращает адрес старницы с товаром
 
@@ -26,7 +26,7 @@ class Triol:
             if 'href="/product' in page_line:
                 return 'https://amma.pet' + page_line[6:-1]
 
-    def get_info_item(self):
+    def get_info_item_amma(self):
         # сбор информации о товаре со траницы
 
         text: str = requests.post(self.link).text \
@@ -47,8 +47,33 @@ class Triol:
                 continue
             text[i] = val[14:-2]
 
-        return text[-3], text[-2], [i for i in text if 'jpg' in i]
+        return text[-3], text[-2], ['https://amma.pet/' + i for i in text if 'jpg' in i]
 
 
 class Product(Triol):
-    pass
+
+    def __init__(self, item):
+        self.id_item = item
+        self.name, self.description = self.get_info_db()
+        self.image = self.get_image_db()
+
+    def get_info_db(self):
+        # Ищет информацию в БД
+
+        con = sqlite3.connect(DataBase)
+        cursor = con.cursor()
+        cursor.execute('select name, description FROM Product where item_number = ' + self.id_item)
+        try:
+            out = cursor.fetchall()[0]
+        except IndexError:
+            return 'Описание товара не найдено'
+
+        return out
+
+    def get_image_db(self):
+        # ищет фото торава по id
+        con = sqlite3.connect(DataBase)
+        cursor = con.cursor()
+
+        cursor.execute('SELECT Image FROM Image WHERE item_number =' + self.id_item)
+        return cursor.fetchone()
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from TRIOL2_lib import *
+from TRIOL2_lib import Product
+import telebot
+from CONFIG import TOKEN
 
-x = Product('12111065')
+import time
 
-x.get_info_item()
+bot = telebot.TeleBot(TOKEN)
 
-print(x.name)
-print(x.description)
-print(x.image)
+@bot.message_handler(content_types=["text"])
+def send_info_product(message):
+    start = time.time()
+    id_item = message.text
+    if len(id_item) > 9:
+        # Ограничение на длину id товара более 9 знаков
+        return 0
+    item = Product(id_item)
+    bot.send_message(message.chat.id, '\n'.join([item.name, item.description]))
+    bot.send_photo(message.chat.id, item.image[0])
+    print(time.time() - start)
+
+if __name__ == '__main__':
+    bot.infinity_polling()
